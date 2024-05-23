bf183ad0917afc141f99ded7119a2a26456804bc
Sarvesh <sarveshjaiofficial@gmail.com>
2024-05-24 01:45:10 +0530

    Updated Backend Deployment link in the backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://mern-authentication-gamma.vercel.app",
+    origin: ["https://mern-authentication-gamma.vercel.app"],
     credentials: true,
   })
 );
