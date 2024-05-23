a4fe3e0dae16e27766c98c5fc8e9f8b71e21133e
Sarvesh <sarveshjaiofficial@gmail.com>
2024-05-24 01:51:04 +0530

    Completed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://mern-authentication-gamma.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
